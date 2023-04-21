850b9cd4b4fd4a6b88e7b313f5bea8cf2ba65561
Gustavo Sousa <gusousa@student.42.rio>
2023-04-21 15:07:22 -0300

    aligned

--- C00/ex01/source/PhoneBook.cpp
+++ C00/ex01/source/PhoneBook.cpp
@@ -32,18 +32,18 @@ void	PhoneBook::search( void ) {
 	PhoneBook::printHeader();
 	for (int i = 0; i < this->totalContacts; i++)
 	{
-		std::cout << std::setw(10) << std::left << i + 1 << " | "	\
-		<< std::setw(10) <<  std::left << this->listContacts[i].getFirstName() << " | "	\
-		<< std::setw(10) <<  std::left << this->listContacts[i].getLastName() << " | "		\
-		<< std::setw(10) <<  std::left << this->listContacts[i].getNickname() << std::endl;
+		std::cout << std::setw(10) << std::left << i + 1 << "|";
+		std::cout << std::setw(10) << std::left << this->listContacts[i].getFirstName() << "|";
+		std::cout << std::setw(10) << std::left << this->listContacts[i].getLastName() << "|";
+		std::cout << std::setw(10) << std::left << this->listContacts[i].getNickname() << std::endl;
 	}
 	std::cout << "type a number to chosse a contact" << std::endl;
 	std::cin >> choice;
 	if (std::isdigit(choice) && choice > 0 && choice <= this->totalContacts)
 	{
-		std::cout << std::setw(10) << this->listContacts[choice - 1].getFirstName() << " | "	\
-		<< std::setw(10) << this->listContacts[choice - 1].getLastName() << " | "		\
-		<< std::setw(10) << this->listContacts[choice - 1].getNickname() << std::endl;
+		std::cout << std::setw(10) <<  std::left << this->listContacts[choice - 1].getFirstName() << "|";
+		std::cout << std::setw(10) <<  std::left << this->listContacts[choice - 1].getLastName() << "|";
+		std::cout << std::setw(10) <<  std::left << this->listContacts[choice - 1].getNickname() << std::endl;
 	}
 }
 

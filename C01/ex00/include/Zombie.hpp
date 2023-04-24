03649c875b81a3cec0754d6952b568fff5335b51
Gustavo Sousa <gusousa@student.42.rio>
2023-04-24 12:01:12 -0300

    New Zombies

---
+++ C01/ex00/include/Zombie.hpp
@@ -0,0 +1,19 @@
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class	Zombie {
+private:
+	std::string _name;
+
+public:
+	Zombie(void);
+	~Zombie(void);
+	void		setName( std::string );
+	std::string	getName( void );
+	void		announce( void );
+};
+
+#endif

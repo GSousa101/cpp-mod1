404fcf09ac8f13b89ecee378953ef8911d361980
Gustavo Sousa <gfsousa42@gmail.com>
2023-04-26 18:05:48 +0000

    Method attack

--- C01/ex03/source/HumanA.cpp
+++ C01/ex03/source/HumanA.cpp
@@ -4,5 +4,5 @@
 HumanA::HumanA( Weapon& weapon) : _weapon(weapon) {}
 
 void	HumanA::attack( void ) {
-	std::cout << this->_name << "attacks with their " << this->_weapon << std::endl;
+	std::cout << this->_name << "attacks with their " << this->_weapon.getType() << std::endl;
 }

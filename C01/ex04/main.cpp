4253473c80114bb3d847c0ab320f7da04d0c8958
Gustavo Sousa <gusousa@student.42.rio>
2023-04-27 11:41:38 -0300

    using find to s1

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -2,9 +2,22 @@
 #include <iostream>
 #include <fstream>
 
+void	replaceLine(std::string line, std::string s1, std::string s2) {
+	int	beginIndex;
+
+(void)s2;
+	beginIndex = line.find(s1);
+	while (beginIndex != -1)
+	{
+
+		std::cout << line << beginIndex << std::endl;
+		beginIndex = line.find(s1);
+
+	}
+}
 int	main (int argc, char** argv) {
-	std:string	s1 = argv[2];
-	std:string	s2 = argv[3];
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
 
 	if (argc != 4)
 		std::cout << "Usage: ./ft_sed <file_name> <string1> <string2>" << std::endl;
@@ -12,15 +25,11 @@ int	main (int argc, char** argv) {
 	{
 		std::ifstream	infile(argv[1]);
 		std::string	line;
-
-		std::getline(infile, line);
-
 		//deal_error file.
-		//replace for new string
-		//	Find
-		//	substitute
 
-
+		while (std::getline(infile, line))
+			replaceLine(line, s1, s2);
+			std::cout << line << std::endl;
 	}
 	return (0);
 }

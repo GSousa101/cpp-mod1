3de48346bee53f2b6e2cc1b1110c54ffcf66891b
Gustavo Sousa <gfsousa42@gmail.com>
2023-04-30 19:48:01 +0000

    New

---
+++ C01/ex06/main.cpp
@@ -0,0 +1,14 @@
+#include "include/Harl.hpp"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+      std::cout << "Usage: ./harlFilter <complain>" << std::endl;
+
+    return ( 1 );
+  }
+  Harl harl;
+
+  std::string complain(argv[1]);
+  harl.complain(complain);
+  return 0;
+}

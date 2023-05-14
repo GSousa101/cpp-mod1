fc79d6e7de00624420090f139392d9a84f58d6cc
Gustavo Sousa <gfsousa42@gmail.com>
2023-05-14 19:27:27 +0000

    First try for AMateria

---
+++ C04/ex03/include/AMateria.hpp
@@ -0,0 +1,29 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <iostream>
+
+//#include "../interfaces/ICharacter.hpp"
+
+class AMateria {
+
+    public:
+        // Constructors and Destructors
+        virtual     AMateria( void );
+                    AMateria(std::string const& type);
+                    AMateria(const AMateria& other);
+        virtual     ~AMateria( void );
+        AMateria&   operator=(const AMateria& other);
+
+        // Getters
+        std::string const&  getType() const;
+
+        // Member Functions
+        virtual AMateria*   clone() const = 0;
+//        virtual void        use(ICharacter& target);
+
+    protected:
+        std::string _type;
+};
+
+#endif

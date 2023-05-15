4243d9ffecc62c052747351d892c080ba44422fa
Gustavo Sousa <gusousa@student.42.rio>
2023-05-15 18:28:59 -0300

    New functions to impplement

--- C02/ex01/include/Fixed.hpp
+++ C02/ex01/include/Fixed.hpp
@@ -8,19 +8,25 @@ class Fixed
 {
 
 	public:
-
+		// Constructions & destructor
 		Fixed( void );
 		Fixed( Fixed const& src );
 		~Fixed( void );
 
+		// Overload operation
 		Fixed &		operator=( Fixed const & rhs );
 
+		// Member functions
 		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
 
 	private:
 		int					_value;
 		int static const	fractionalBits;
 };
+
+std::ostream &operator<<(std::ostream &stream, Fixed const &other);
 
 #endif

7a776bca24add5aaa8d04c3a6122715affe25289
Gustavo Sousa <gusousa@student.42.rio>
2023-05-18 12:00:50 -0300

    lil fix

--- C03/ex00/main.cpp
+++ C03/ex00/main.cpp
@@ -2,8 +2,19 @@
 
 int main( void )
 {
-    ClapTrap jack("Jack");
-    ClapTrap dom;
+    {
+        ClapTrap dom;
 
-    dom.beRepaired(2);
+        dom.attack("Moinho de vento");
+        dom.takeDamage(4);
+        dom.takeDamage(2);
+        dom.takeDamage(1);
+        dom.takeDamage(1);
+        dom.beRepaired(2);
+        dom.attack("Robbers");
+        dom.attack("inners");
+        dom.takeDamage(6);
+        dom.takeDamage(6);
+        dom.beRepaired(1);
+    }
 }
--- C03/ex00/source/ClapTrap.cpp
+++ C03/ex00/source/ClapTrap.cpp
@@ -6,7 +6,7 @@
 
 ClapTrap::ClapTrap( void )
 {
-	this->_name = "Dom Quixote";
+	this->_name = "Don Quixote";
 	this->_hitPoints = 10;
 	this->_energyPoints = 10;
 	this->_attackDamage = 0;
@@ -63,33 +63,36 @@ void				message(std::string str) { std::cout << str << std::endl; }
 void				ClapTrap::attack(std::string const& target)
 {
 	if (this->_energyPoints == 0)
-		return message(this->_name + " don't have enough points to attack");
+		return message(RED + this->_name + " don't have enough points to attack." + RESET);
 	if (this->_hitPoints == 0)
-		return message(this->_name + " is dead, cannot attack");
+		return message(RED + this->_name + " is dead, cannot attack." + RESET);
 	this->_energyPoints--;
-	std::cout << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+	std::cout << RED << this->_name << " attacks " << target << ", causing ";
+	std::cout << this->_attackDamage << " points of damage!" << RESET << std::endl;
 }
 
 void				ClapTrap::takeDamage(unsigned int amount)
 {
 	if (this->_hitPoints == 0)
-		return message(this->_name + " is already dead.");
+		return message(ORANGE + this->_name + " is already dead." + RESET);
 	if (amount >= this->_hitPoints)
 	{
 		this->_hitPoints = 0;
-		return message(this->_name + " receives an attack and died in the spot!");
+		return message(ORANGE + this->_name + " receives an attack and died in the spot!" + RESET);
 	}
 	this->_hitPoints -= amount;
-	std::cout << this->_name << " receives an attack, causing " << amount << " of damage. Points left: " << this->_hitPoints;
+	std::cout << ORANGE << this->_name << " receives an attack, causing ";
+	std::cout << amount << " of damage. Points left: " << this->_hitPoints << RESET << std::endl;
 }
 
 void				ClapTrap::beRepaired(unsigned int amount)
 {
 	if (this->_hitPoints <= 0)
-		message (this->_name + " can't be repaired because the soul has left the body!");
+		return message (CYAN + this->_name + " can't be repaired because the soul has left the body!" + RESET);
 	if (this->_energyPoints <= 0)
-		message(this->_name + " can't be repaired because doesn't have enough energyPoints!");
+		return message(CYAN + this->_name + " can't be repaired because doesn't have enough energyPoints!" + RESET);
 	this->_hitPoints += amount;
 	this->_energyPoints--;
-	std::cout << this->_name << " repair " << amount << " hitpoints. Total: " << this->_hitPoints << std::endl;
+	std::cout << CYAN << this->_name << " repair " << amount << " hitpoints. Points left: ";
+	std::cout << this->_hitPoints << RESET << std::endl;
 }

c5e4e8a8842ebe7a82b4476355161890cc366a6c
Gustavo Sousa <gfsousa42@gmail.com>
2023-06-21 10:01:39 -0300

    new files for ex01

---
+++ C05/ex01/include/Color.hpp
@@ -0,0 +1,13 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#define CYAN "\033[0;36m"
+#define GREEN "\033[0;32m"
+#define ORANGE "\033[1;38;5;214m"
+#define RED "\033[0;31m"
+#define RESET "\033[0m"
+#define YELLOW "\033[0;33m"
+#define MAGENTA "\033[1;35m"
+#define BLACK "\033[0;30m"
+
+#endif
---
+++ C05/ex01/source/Bureaucrat.cpp
@@ -0,0 +1,93 @@
+#include "../include/Bureaucrat.hpp"
+
+/*
+** --------------------------- CONSTRUCTOR & DESTRUCTOR -----------------------------
+*/
+Bureaucrat::Bureaucrat( void ) : _name("John Wick")
+{
+	this->_grade = 150;	
+}
+
+Bureaucrat::Bureaucrat( std::string newName, int grade) : _name(newName)
+{
+	if (grade < 1)
+		throw(Bureaucrat::GradeTooHighException());
+	if (grade > 150)
+		throw(Bureaucrat::GradeTooLowException());
+	this->_grade = grade;
+}
+
+Bureaucrat::Bureaucrat( Bureaucrat const& other )
+{
+	this->_name = other._name;
+	this->_grade = other._grade;
+}
+
+Bureaucrat::~Bureaucrat() { }
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+Bureaucrat&			Bureaucrat::operator=( Bureaucrat const& other )
+{
+	if ( this != &other )
+	{
+		this->_name = other._name;
+		this->_grade = other._grade;
+	}
+	return *this;
+}
+
+/*
+**	------------------------- GETTERS ------------------------
+*/
+std::string			Bureaucrat::getName( void ) const
+{
+	return this->_name;
+}
+
+int					Bureaucrat::getGrade( void ) const
+{
+	return this->_grade;
+}
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+void				Bureaucrat::incrementGrade( void )
+{
+	if (this->_grade == 1)
+		throw(Bureaucrat::GradeTooHighException());
+	std::cout << CYAN << "Incrementing " << this->_name << "'s grade" << RESET << std::endl;
+	this->_grade--;
+}
+
+void				Bureaucrat::decrementGrade( void )
+{
+	if (this->_grade == 150)
+		throw(Bureaucrat::GradeTooLowException());
+	std::cout << CYAN << "Decrementing " << this->_name << "'s grade" << RESET << std::endl;
+	this->_grade++;
+}
+
+/*
+**	---------------------------- CUSTOMS EXCEPTIONS ---------------------------
+*/
+const char*			Bureaucrat::GradeTooLowException::what( void ) const throw()
+{
+	return (RED "Grade is too low" RESET);
+}
+
+const char*			Bureaucrat::GradeTooHighException::what( void ) const throw()
+{
+	return (RED "Grade is too high" RESET);
+}
+
+/*
+**	---------------------------- GLOBAL OVERLOAD ---------------------------
+*/
+std::ostream&		operator<<( std::ostream& o, Bureaucrat const& person)
+{
+	o << person.getName() << ", bureaucrat grade " << person.getGrade() << ".";
+	return o;
+}

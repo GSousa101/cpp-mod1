6ac943f11566216a1210a581518fa183e4c298a4
Gustavo Sousa <gfsousa42@gmail.com>
2023-06-22 20:47:01 -0300

    newfiles

---
+++ C05/ex03/main.cpp
@@ -0,0 +1,32 @@
+#include "include/Bureaucrat.hpp"
+
+int main( void )
+{
+{
+	Bureaucrat	Jordan;
+	try 
+	{
+		std::cout << YELLOW << "FIRST TEST" << RESET << std::endl;	
+		std::cout << Jordan << std::endl;
+
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+{
+	Bureaucrat James("James Duck", 3);
+	try
+	{
+		std::cout << std::endl << YELLOW << "SECOND TEST" << RESET << std::endl;
+		std::cout << James << std::endl;
+	
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+	return (0);
+}

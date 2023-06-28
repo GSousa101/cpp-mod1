ec45a5464ddacba94a65c4f4dcbe12a28e993b3b
gusousa <gusousa@student.42.rio>
2023-06-28 16:21:38 -0300

    feat: main to test

---
+++ C06/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "include/Scalar.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std:cerr << "Invalid parameter. usage: ./convert <param>" << std::end;
+            return (1);
+    }
+    Scalar::convert(argv[1]);
+    return (0);
+}

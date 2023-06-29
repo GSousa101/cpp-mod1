cce23b3bef7885c25e1d7c2c513acced86760d70
Gustavo Sousa <gfsousa42@gmail.com>
2023-06-29 20:57:39 -0300

    chore: prepare test

--- C06/ex01/main.cpp
+++ C06/ex01/main.cpp
@@ -1,12 +1,7 @@
-#include "include/Scalar.hpp"
+#include "include/Serializer.hpp"
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        std::cerr << "Invalid parameter. usage: ./convert <param>" << std::endl;
-            return (1);
-    }
-    Scalar::convert(argv[1]);
+
     return (0);
 }

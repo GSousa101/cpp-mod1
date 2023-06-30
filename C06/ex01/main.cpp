17bb1b72c5f8b70615da3b5b9a877c998b9e687a
Gustavo Sousa <gfsousa42@gmail.com>
2023-06-30 14:23:01 -0300

    fix: argument calling funciont

--- C06/ex01/main.cpp
+++ C06/ex01/main.cpp
@@ -7,8 +7,8 @@ int main( void )
 
     my_data->serialNumber = 789456123;
 
-    my_data = Serializer::serialize(my_size_pointer);
-    if ( Serializer::deserialize(my_data) == my_data)
+    my_size_pointer = Serializer::serialize(my_data);
+    if ( Serializer::deserialize(my_size_pointer) == my_data)
         std::cout << "It was serialized and deseliazed with success" << std::endl;
     else
         std::cout << "Some information was missed" << std::endl;

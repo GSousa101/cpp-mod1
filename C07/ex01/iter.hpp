a20ff9a478b32f7163789a7e57b5c551941bfa29
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-04 10:59:15 -0300

    complet ex01

--- C07/ex01/iter.hpp
+++ C07/ex01/iter.hpp
@@ -1,8 +1,10 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+
 template <typename T>
-void    iter(T* array, size_t len, void (*ft)(const& T))
+void    iter(T* array, size_t len, void (*ft)(T const& arg))
 {
     for (size_t i = 0; i < len; i++)
         ft(array[i]);

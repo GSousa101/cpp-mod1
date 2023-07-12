fe5ad9fc1b94238a68cfa4aa7901c04579f0d5b5
gusousa <gusousa@student.42.rio>
2023-07-12 19:26:09 -0300

    new add Range

--- C08/ex01/span.cpp
+++ C08/ex01/span.cpp
@@ -1,6 +1,6 @@
 #include "span.hpp"
 
-Span::Span( void ) {
+Span::Span( void ) : _myVector(NULL), _lenght(NULL) {
 }
 
 Span::Span(int lenght) : _lenght(lenght), _myVector(NULL)
@@ -30,7 +30,27 @@ Span&   Span::operator=(Span const& other) {
 
 void    Span::addNumber( int nbr )
 {
-    if (nbr > maxLenght)
-        throw OutOfRange();
-    
+    if (this->_myVector.size() > this->_maxLenght)
+        throw MaxLenghtReached();
+    this->_myVector.push_back(nbr);
+}
+
+void    Span::addRange(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+    int newSize = std::distance(begin, end) + this->_myVector.size();
+
+    if (newSize > this->_lenght)
+        throw MaxLenghtReached();
+    this->_myVector.insert(this->_myVector.size(), begin, end);
+}
+
+int     Span::shortestSpan() {
+
+}
+
+int     Span::longestSpan() {
+
+}
+
+const char*     Span::MaxLenghtReached::what() const throw() {
+    return "The vector has reached the maximum."
 }

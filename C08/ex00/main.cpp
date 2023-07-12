d7bfc3893cd6f11c3cd816255d98f740451b5040
gusousa <gusousa@student.42.rio>
2023-07-12 09:52:00 -0300

    fix:  calling function the right way

--- C08/ex00/main.cpp
+++ C08/ex00/main.cpp
@@ -8,22 +8,22 @@ int main( void ) {
         for (int i = 0; i < 5; i++) {
             g1.push_back(i);
         }
-        std::vector<int>::iterator nbr = easyfind<std::vector>(g1, 4);
-        std::cout << "Number four of vector is at: " << std::vector<int>::nbr << std::endl;
+        std::vector<int>::iterator nbr = easyfind(g1, 4);
+        std::cout << "Number four of vector is at: " << *nbr << std::endl;
 
         std::list<int>   g2;
         for (int i = 0; i < 10 ; i++) {
             g2.push_back(i);
         }
-        nbr = easyfind<std::list>(g2, 7);
-        std::cout << "Number seven of list is at: " << std::list<int>::nbr << std::endl;
+        std::list<int>::iterator nbr2 = easyfind(g2, 7);
+        std::cout << "Number seven of list is at: " << *nbr2 << std::endl;
 
         std::deque<int>     g3;
         for (int i = 0; i < 20 ; i++) {
             g2.push_front(i);
         }
-        nbr = easyfind<std::deque>(g3, 20);
-        std::cout << "Number twenty (non-exit) of list is at: " << std::deque<int>::nbr << std::endl;
+        std::deque<int>::iterator nbr3 = easyfind(g3, 20);
+        std::cout << "Number twenty (non-exit) of list is at: " << *nbr3 << std::endl;
 
     }
     catch (std::exception& e)

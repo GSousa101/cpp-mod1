6cc9c107815f6f413c4c0404edef74b67b367f2f
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-17 11:52:19 -0300

    new tests

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -4,20 +4,33 @@
 int main( void ) {
     try 
     {
+        std::cout << "------- FIRST TEST -------" << std::endl;
         Span sp = Span(5);
-
         sp.addNumber(6);
-        // sp.addRange(3);
-        // sp.addNumber(3);
-        // sp.addNumber(17);
-        // sp.addNumber(9);
-        // sp.addNumber(11);
-        // std::cout << sp.shortestSpan() << std::endl;
-        // std::cout << sp.longestSpan() << std::endl;
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
     }
     catch (std::exception& e)
     {
         std::cerr << e.what() << std::endl;
     }
+    try
+    {
+        std::cout << std::endl << "------- FIRST TEST -------" << std::endl;
+        Span slots(100);
+        std::vector<int> numbers;
+        for (int i = 0; i < 100; i++) {
+          numbers.push_back(rand() % 100);
+        }
+        slots.addRange(numbers.begin(), numbers.end());
+        std::cout << slots.shortestSpan() << std::endl;
+        std::cout << slots.longestSpan() << std::endl;
+  } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+  }
     return (0);
 }

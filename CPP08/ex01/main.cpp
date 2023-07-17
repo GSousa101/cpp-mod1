7a3b104b6d46d57c9b0ae4d91889c74f0c0dd145
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-17 15:10:26 -0300

    feat: tests:

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -27,12 +27,27 @@ int main( void ) {
         for (int i = 0; i < 990; i++) {
           numbers.push_back(i);
         }
-        numbers.push_back(9000);
         slots.addRange(numbers.begin(), numbers.end());
         std::cout << "My shortie: " << slots.shortestSpan() << std::endl;
         std::cout << "My longgie " << slots.longestSpan() << std::endl;
-  } catch (std::exception& e) {
+    }
+    catch (std::exception& e) {
         std::cout << e.what() << std::endl;
-  }
+    }
+    try
+    {
+        std::cout << std::endl << "------- THIRD TEST -------" << std::endl;
+        Span slots(100);
+        std::vector<int> numbers;
+        for (int i = 0; i < 100; i++) {
+          numbers.push_back(i);
+        }
+        slots.addRange(numbers.begin(), numbers.end());
+        std::cout << "My shortie: " << slots.shortestSpan() << std::endl;
+        std::cout << "My longgie " << slots.longestSpan() << std::endl;
+    }
+        catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
     return (0);
 }

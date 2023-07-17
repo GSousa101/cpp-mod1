7ecabf67518b1157139af90dcaa490338d2d2dec
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-17 14:54:26 -0300

    Tets okay

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -19,19 +19,20 @@ int main( void ) {
         std::cerr << e.what() << std::endl;
     }
 
-  //   try
-  //   {
-  //       std::cout << std::endl << "------- SECOND TEST -------" << std::endl;
-  //       Span slots(100);
-  //       std::vector<int> numbers;
-  //       for (int i = 0; i < 100; i++) {
-  //         numbers.push_back(rand() % 100);
-  //       }
-  //       slots.addRange(numbers.begin(), numbers.end());
-  //       std::cout << "My shortie: " << slots.shortestSpan() << std::endl;
-  //       std::cout << "My longgie " << slots.longestSpan() << std::endl;
-  // } catch (std::exception& e) {
-  //       std::cout << e.what() << std::endl;
-  // }
+    try
+    {
+        std::cout << std::endl << "------- SECOND TEST -------" << std::endl;
+        Span slots(100);
+        std::vector<int> numbers;
+        for (int i = 0; i < 990; i++) {
+          numbers.push_back(i);
+        }
+        numbers.push_back(9000);
+        slots.addRange(numbers.begin(), numbers.end());
+        std::cout << "My shortie: " << slots.shortestSpan() << std::endl;
+        std::cout << "My longgie " << slots.longestSpan() << std::endl;
+  } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+  }
     return (0);
 }

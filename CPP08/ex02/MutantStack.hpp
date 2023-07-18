487f1cb6e67110d44187b64a2f7843876e9cb405
gusousa <gusousa@student.42.rio>
2023-07-18 14:26:35 -0300

    feat: logic to ex

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,16 +1,31 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-template <typename T>
-class MutantStack {
-public:
-    MutantStack();
-    MutantStack(const MutantStack &);
-    MutantStack &operator=(const MutantStack &);
-    virtual ~MutantStack();
+# include <iostream>
+# include <stack>
 
-private:
-    
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() { };
+        MutantStack(const MutantStack& other) : std::stack<t>(other) { };
+        MutantStack &operator=(const MutantStack & other) {
+            if (this != *other)
+                std::stack<T>:operator=(other);
+            return *this;
+        };
+        virtual ~MutantStack() { };
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator            begin( void ) { return this->c.begin(); }
+        iterator            end( void ) { return this->c.end(); };
+        
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        const_iterator      begin( void ) const { return this->c.begin(); }
+        const_iterator      end( void ) const { return this->c.end(); }
 };
+
+#include "MutantStack.tpp"
 
 #endif
--- CPP08/ex02/MutantStack.tpp
+++
@@ -1,4 +0,0 @@
-
-#include "MutantStack.hpp"
-
-

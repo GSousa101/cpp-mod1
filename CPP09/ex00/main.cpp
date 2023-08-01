a7ed5b7d438029c3623aac7a34981e5547bf5fb4
Gustavo Sousa <gfsousa42@gmail.com>
2023-08-01 16:43:25 -0300

    newTests

---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,22 @@
+#include "BitcoinExchange.hpp"
+
+bool isValidFile(std::string const& filename)
+{
+    std::ifstream inputFile(filename);
+    if (!inputFile
+        || inputFile.peek() == std::ifstream::traits_type::eof())
+    {
+        return false;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2 || !isValidFile(argv[1])) {
+        std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    BitcoinExchange btc;
+    btc.action(argv[1]);
+    return 0;
+}
